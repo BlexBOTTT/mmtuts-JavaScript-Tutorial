45ee7531cf954368d518b740f19064d0691605a5
Blix Quirante <92987751+BlexBOTTT@users.noreply.github.com>
2023-05-21 22:58:12 +0800

    Video 6

---
+++ 6 Rules for Writing JavaScript Code/script.js
@@ -0,0 +1,21 @@
+document.body.innerHTML = "test text 123123";
+
+// text txt comment single liner
+
+/*test comment MediaQu
+m ulti-liner comments */
+
+// This changes the menu color
+
+var names = "Naema";
+
+// javascript case sensitivers
+alert(names);
+
+var nameList = "";
+
+var name = new Object();
+
+const NAME = "test 123123";
+
+document.body.innerHTML = "text texxt"; var name = "Naema 2";
